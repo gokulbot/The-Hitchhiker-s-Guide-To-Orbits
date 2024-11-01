0bb6ad9fbb483eca8918a80e36e8d0ab70eff041
gsanthosh@monarchtractor.com <gsanthosh@monarchtractor.com>
2024-11-01 19:34:05 +0530

    add hooks

---
+++ test/example.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int a = 10;
+  int b = 20;
+  int sum = a + b;
+  cout << "The sum is: " << sum << endl;
+  if (a < b) {
+    cout << "a is less than b" << endl;
+  } else {
+    cout << "a is not less than b" << endl;
+  }
+  return 0;
+}
---
+++ test/example.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    sum = a + b
+    print("The sum is:", sum)
+
+
+a = 10
+b = 20
+add(a, b)
+if a < b:
+    print("a is less than b")
+else:
+    print("a is not less than b")
